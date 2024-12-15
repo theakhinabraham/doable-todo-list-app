e175dd196548d7d00dce63b025a15bb5eae0888a
Luis Schröder <kresstudios@gmail.com>
2024-12-15 15:01:58 +0100

    issue #2 fixed and the UI for the Settings page is finished

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:doable_todo_list_app/widgets/back_arrow.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -5,7 +6,101 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface.withOpacity(0.5),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 100),
+          child: currentWidth < 600 ? Text(
+              'Settings',
+              style: TextStyle(
+                fontWeight: FontWeight.w500
+                ),
+              ) : null,
+        ),
+      ),
 
-    return const Scaffold();
+      body: Column(
+        children: [
+
+          SizedBox(height: 15,),
+
+          // Allow Notifications Switch
+          Center(
+               child: currentWidth < 600 ? Container(
+                padding: const EdgeInsets.all(25),
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 1, color: Colors.black),
+                ),
+                child: Row(
+                  children: [
+                  Text(
+                  'Allow Notifications', 
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black
+                    ),
+                   ),
+
+                   SizedBox(width: 120),
+
+                   Switch(
+                    value: true, 
+                    onChanged: null,
+                    ),
+                  ],
+                ),
+                           ) : null,
+             ),
+
+             SizedBox(height: 15,),
+
+          // Delete User Data
+          Center(
+               child: Container(
+                padding: const EdgeInsets.all(25),
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black
+                ),
+                child: Row(
+                  children: [
+                  Text(
+                  'Clear All Data', 
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white
+                    ),
+                   ),
+
+                   SizedBox(width: 180),
+                   Icon(
+                    Icons.arrow_forward_rounded, 
+                    ),
+                  ],
+                ),
+                           ),
+             ),
+
+             SizedBox(height: 15,),
+
+             Divider(endIndent: 45, indent: 45,),
+
+             SizedBox(height: 15,),
+
+             // GitHub name
+             SelectableText(
+              "GitHub: @theakhinabraham",
+              style: TextStyle(
+              ),
+             ),
+             
+        ],
+      ),
+    );
   }
 }
